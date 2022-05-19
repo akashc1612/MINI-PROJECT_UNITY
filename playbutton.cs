2cc42529e43fa35293b13df612b5fb141442f8cf
ras733171 <ras733171@gmail.com>
2022-05-19 23:06:20 +0530

    second

---
+++ playbutton.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playbutton : MonoBehaviour
+{
+    public void playgame(){
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);}
+}
